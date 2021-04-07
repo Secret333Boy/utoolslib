d572cd2b3907b79e8374d390cd07ecc2b7a1b18a
GrigoriyUstymenko <gromgoriy@gmail.com>
2021-04-07 10:39:56 +0300

    Created Dictionary class with basic methods

--- src/tools/spellChecker/spellChecker.js
+++ src/tools/spellChecker/spellChecker.js
@@ -1,8 +1,29 @@
 'use strict';
+class Dictionary {
+  constructor(words = []) {
+    this._words = words.sort();
+  }
+
+  push(words) {
+    this._words = words.concat(this._words);
+    this._words.sort();
+    return this;
+  }
+
+  clear() {
+    const words = this._words;
+    this._words = [];
+    return words;
+  }
+
+  get words() {
+    return this._words;
+  }
+}
 
 class spellChecker {
   constructor(words = []) {
-    this.dictionary = words;
+    this.dictionary = new Dictionary(words);
   }
 }
 
