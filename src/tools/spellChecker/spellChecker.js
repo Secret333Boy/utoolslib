c72fee21e103a9064640a4ac356aba5d6780f334
GrigoriyUstymenko <gromgoriy@gmail.com>
2021-04-07 10:51:35 +0300

    Added length property to dictionary

--- src/tools/spellChecker/spellChecker.js
+++ src/tools/spellChecker/spellChecker.js
@@ -1,11 +1,14 @@
 'use strict';
+
 class Dictionary {
   constructor(words = []) {
     this._words = words.sort();
+    this._length = words.length;
   }
 
   push(words) {
     this._words = words.concat(this._words);
+    this._length += words.length;
     this._words.sort();
     return this;
   }
@@ -13,11 +16,16 @@ class Dictionary {
   clear() {
     const words = this._words;
     this._words = [];
+    this._length = 0;
     return words;
   }
 
   get words() {
     return this._words;
+  }
+
+  get length() {
+    return this._length;
   }
 }
 
