8d6688a6fa9965f2863687ae3625477d3159a088
GrigoriyUstymenko <gromgoriy@gmail.com>
2021-04-12 17:38:04 +0300

    Added test cases

--- tests/spellChecker.js
+++ tests/spellChecker.js
@@ -8,7 +8,51 @@ const dictPath = 'tests/spellCheckerDict.txt';
 const wordRegex = /[a-z]+/gi;
 const testDict = fs.readFileSync(dictPath, 'utf8').match(wordRegex);
 
-const tests = {};
+const tests = {
+  check: [
+    [
+      'This sentence should be the same on exit because ' +
+        'it does not have any spelling mistakes (hopefully).',
+      'Correct string',
+      'This sentence should be the same on exit because ' +
+        'it does not have any spelling mistakes (hopefully).',
+      3,
+    ],
+    [
+      'This sentence has some mistakes, but it should not ' +
+        'be a problem. Also, here is another sentence!',
+      'A few sentences with some mistakes',
+      'This sentense has some mistaces, but it shud not ' +
+        'be a problemm. Also, here is anoter sentenc!',
+      3,
+    ],
+  ],
+  _calcDiff: [
+    [1, 'One letter diff', 'cherleader', 'cheerleader'],
+    [0, 'Same word', 'tree', 'tree'],
+    [0, 'Scrambled word', 'property', 'roptrype'],
+    [0, 'Partly scrambled word', 'trespassing', 'terspassign'],
+    [3, 'Three letter diff', 'asemblli', 'assembly'],
+    [6, 'Longer word', 'ring', 'ringleader'],
+  ],
+  _parseWords: [
+    [
+      [
+        'These',
+        'words',
+        'will',
+        'turn',
+        'into',
+        'an',
+        'array',
+        'just',
+        'watch',
+      ],
+      'One sentence parsing',
+      'These words will turn into an array, just watch.',
+    ],
+  ],
+};
 
 testRunner: {
   const spellChecker = new SpellChecker(testDict);
@@ -20,7 +64,7 @@ testRunner: {
     if (tests[method] === undefined) continue;
 
     for (const test of tests[method]) {
-      const [pars, expected, name] = test;
+      const [expected, name, ...pars] = test;
       const result = spellChecker[method](...pars);
       try {
         assert.strictEqual(
