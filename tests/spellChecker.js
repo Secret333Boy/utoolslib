d0d6d87149ded761e72b72e588be054af3d9b374
GrigoriyUstymenko <gromgoriy@gmail.com>
2021-04-12 16:59:52 +0300

    Implemented the test runner

--- tests/spellChecker.js
+++ tests/spellChecker.js
@@ -1,4 +1,36 @@
 'use strict';
 
-const assert = require('assert');
+const assert = require('assert').strict;
+const fs = require('fs');
 const SpellChecker = require('../src/tools/spellChecker/spellChecker.js');
+
+const dictPath = 'tests/spellCheckerDict.txt';
+const wordRegex = /[a-z]+/gi;
+const testDict = fs.readFileSync(dictPath, 'utf8').match(wordRegex);
+
+const tests = {};
+
+testRunner: {
+  const spellChecker = new SpellChecker(testDict);
+  const methods = Object.getOwnPropertyNames(SpellChecker.prototype);
+
+  for (const method of methods) {
+    if (method === 'constructor') continue;
+    if (typeof spellChecker[method] !== 'function') continue;
+    if (tests[method] === undefined) continue;
+
+    for (const test of tests[method]) {
+      const [pars, expected, name] = test;
+      const result = spellChecker[method](...pars);
+      try {
+        assert.strictEqual(
+          result,
+          expected,
+          `Error in method ${method} in test "${name}"`
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+}
