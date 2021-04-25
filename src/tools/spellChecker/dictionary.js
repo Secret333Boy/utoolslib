89fc5b8edf1d5f3888dcca4c753ebf13f7f48f2c
GrigoriyUstymenko <gromgoriy@gmail.com>
2021-04-25 21:41:56 +0300

    Created separate modules for every class

---
+++ src/tools/spellChecker/dictionary.js
@@ -0,0 +1,32 @@
+'use strict';
+
+class Dictionary {
+  constructor(words = []) {
+    this._words = words.sort();
+    this._length = words.length;
+  }
+
+  push(words) {
+    this._words = words.concat(this._words);
+    this._length += words.length;
+    this._words.sort();
+    return this;
+  }
+
+  clear() {
+    const words = this._words;
+    this._words = [];
+    this._length = 0;
+    return words;
+  }
+
+  get words() {
+    return this._words;
+  }
+
+  get length() {
+    return this._length;
+  }
+}
+
+module.exports = Dictionary;
