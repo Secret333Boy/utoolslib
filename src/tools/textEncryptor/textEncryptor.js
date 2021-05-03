aaefb74b2a06cc513fdef1c08290ba6438e23339
Secret333Boy <pashkovskiy.eugen@gmail.com>
2021-05-03 07:16:15 +0300

    modifyString method implementation, minor improvements

--- src/tools/textEncryptor/textEncryptor.js
+++ src/tools/textEncryptor/textEncryptor.js
@@ -11,29 +11,27 @@ class TextEncryptor {
     return code ^ (this._seed ^ 1 ^ this._enternalSeed);
   }
 
+  modifyString(data) {
+    const newData = [];
+    for (let i = 0; i < data.length; i++) {
+      let code = data[i].charCodeAt(0);
+      code = this._modifyCode(code);
+      newData.push(String.fromCharCode(code));
+    }
+    return newData.join('');
+  }
+
   encryptFile(path) {
     fs.readFile(path, 'utf-8', (err, data) => {
       if (err) throw err;
-      const newData = [];
-      for (let i = 0; i < data.length; i++) {
-        let code = data[i].charCodeAt(0);
-        code = this._modifyCode(code);
-        newData.push(String.fromCharCode(code));
-      }
-      fs.writeFileSync(path, newData.join(''));
+      fs.writeFileSync(path, this.modifyString(data));
     });
   }
 
   decryptFile(path) {
     fs.readFile(path, 'utf-8', (err, data) => {
       if (err) throw err;
-      const newData = [];
-      for (let i = 0; i < data.length; i++) {
-        let code = data[i].charCodeAt(0);
-        code = this._modifyCode(code);
-        newData.push(String.fromCharCode(code));
-      }
-      fs.writeFileSync(path, newData.join(''));
+      fs.writeFileSync(path, this.modifyString(data));
     });
   }
 }
