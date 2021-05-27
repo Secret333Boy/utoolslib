45cb3dd57d933c1d5407ea9a72c31d0de3aa0a88
Secret333Boy <pashkovskiy.eugen@gmail.com>
2021-05-27 23:08:02 +0300

    Expaned graph methods

--- src/tools/dataOrganizer/graph.js
+++ src/tools/dataOrganizer/graph.js
@@ -10,6 +10,20 @@ class Graph {
   isConnected(v1, v2) {
     return this.matrix[v1][v2] !== 0;
   }
+
+  isGraph(obj) {
+    return obj instanceof Graph;
+  }
+
+  connect(v1, v2, weight = 0) {
+    this.matrix[v1][v2] = 1;
+    this.weights[v1][v2] = weight;
+  }
+
+  disconnect(v1, v2) {
+    this.matrix[v1][v2] = 0;
+    this.weights[v1][v2] = 0;
+  }
 }
 
 module.exports = Graph;
