2bad1740a07dc37a0e938f77db3ffefdab4e9588
Secret333Boy <pashkovskiy.eugen@gmail.com>
2021-05-30 22:34:00 +0300

    Linter config fix, implemented calculation of table sizes

--- src/tools/dataStructures/table.js
+++ src/tools/dataStructures/table.js
@@ -3,10 +3,26 @@
 class Table {
   constructor(arr) {
     this.map = new Map(arr);
+    this.rawsNum = this.map.entries.length;
+    this.colsNum = this._calcCols(arr);
   }
 
   static isTable(obj) {
     return obj instanceof Table;
+  }
+
+  _calcCols(arr) {
+    let res = 2;
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        res++;
+        const tempArr = arr[i];
+        arr = arr.slice(0, i);
+        arr = arr.concat(tempArr);
+        i--;
+      }
+    }
+    return res;
   }
 }
 
