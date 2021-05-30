6bd18ead996d95b655af1795e0dde909f94a687f
Secret333Boy <pashkovskiy.eugen@gmail.com>
2021-05-30 21:13:29 +0300

    Implemented object with symbols

--- src/tools/dataOrganizer/drawer.js
+++ src/tools/dataOrganizer/drawer.js
@@ -2,14 +2,21 @@
 const fs = require('fs');
 
 class Drawer {
+  static SYMBOLS = {
+    corners: { topLeft: '┌', topRight: '┐', bottomLeft: '└', bottomRight: '┘' },
+    lines: { horizontal: '―', vertical: '│' },
+    ternaryLines = { top: '┬', left: '├', right: '┤', bottom: '┴' },
+  };
   constructor(obj, mode) {
     this.mode = mode;
     this.stringImage = this._getImage(obj);
   }
 
-  // _getImage(obj) {
+  _getImage(obj) {
+    if (mode === 'table' || !mode) {
 
-  // }
+    }
+  }
 
   draw(path) {
     try {
