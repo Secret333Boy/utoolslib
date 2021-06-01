0bfbce71e26ac9ab547ab5c233e435e00e565362
Secret333Boy <pashkovskiy.eugen@gmail.com>
2021-06-01 04:28:33 +0300

    Refactored table class

--- src/tools/dataStructures/table.js
+++ src/tools/dataStructures/table.js
@@ -1,28 +1,16 @@
 'use strict';
 
 class Table {
-  constructor(arr) {
-    this.map = new Map(arr);
-    this.rawsNum = this.map.entries.length;
-    this.colsNum = this._calcCols(arr);
+  constructor(obj) {
+    this.obj = obj;
+    this.rawsNum = Object.keys(obj).length;
+    this.colsNum = Object.keys(obj).reduce((acc, key) =>
+      (Array.isArray(obj[key]) ? obj[key] > acc : acc)
+    );
   }
 
   static isTable(obj) {
     return obj instanceof Table;
-  }
-
-  _calcCols(arr) {
-    let res = 2;
-    for (let i = 0; i < arr.length; i++) {
-      if (Array.isArray(arr[i])) {
-        res++;
-        const tempArr = arr[i];
-        arr = arr.slice(0, i);
-        arr = arr.concat(tempArr);
-        i--;
-      }
-    }
-    return res;
   }
 }
 
