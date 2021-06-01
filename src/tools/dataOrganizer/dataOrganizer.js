d1d92667fbe5f5e31e7a6f04d18117d4aeb28664
Secret333Boy <pashkovskiy.eugen@gmail.com>
2021-06-02 00:13:43 +0300

    Simple dataOrganizer implementation for tables

--- src/tools/dataOrganizer/dataOrganizer.js
+++ src/tools/dataOrganizer/dataOrganizer.js
@@ -1,1 +1,24 @@
 'use strict';
+const Drawer = require('./drawer');
+const Table = require('../dataStructures/table.js');
+
+class DataOrganizer {
+  constructor(settings = {}) {
+    this.settings = settings;
+  }
+
+  drawString(obj, path = '') {
+    let mode;
+    if (Table.isTable(obj)) {
+      mode = 'table';
+    }
+    const drawer = new Drawer(obj, mode);
+
+    if (path) {
+      drawer.draw(path);
+    }
+    return drawer.stringImage;
+  }
+}
+
+module.exports = DataOrganizer;
