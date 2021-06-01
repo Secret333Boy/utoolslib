fc3c11f9c00aaf2d4432cb2d4d917b6ce9351d03
Secret333Boy <pashkovskiy.eugen@gmail.com>
2021-06-01 04:46:41 +0300

    Table cols calculation decomposed

--- src/tools/dataStructures/table.js
+++ src/tools/dataStructures/table.js
@@ -4,11 +4,16 @@ class Table {
   constructor(obj) {
     this.obj = obj;
     this.rawsNum = Object.keys(obj).length;
-    this.colsNum = Object.keys(obj).reduce((acc, key) =>
-      ((Array.isArray(obj[key]) && obj[key] > acc) ? obj[key] : acc)
-    );
-  }
+    this.colsNum = Object.keys(obj).reduce((acc, key) => {
+      const el = obj[key];
 
+      if (Array.isArray(el) && el.length + 1 > acc) {
+        return el.length + 1;
+      } else {
+        return acc;
+      }
+    }, 2);
+  }
   static isTable(obj) {
     return obj instanceof Table;
   }
