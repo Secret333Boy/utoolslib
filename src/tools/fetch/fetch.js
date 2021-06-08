5a98109204c594ebd1aa8d1535264fb362d2c744
GrigoriyUstymenko <gromgoriy@gmail.com>
2021-06-08 16:44:17 +0300

    Added error handling for fetch

--- src/tools/fetch/fetch.js
+++ src/tools/fetch/fetch.js
@@ -1,23 +1,35 @@
 'use strict';
 const http = require('node:https');
 
-const fetch = async url => new Promise((resolve, reject) => {
-  http.get(url, res => {
-    const statusCode = res.statusCode;
+const fetch = async url =>
+  new Promise((resolve, reject) => {
+    http
+      .get(url, res => {
+        const statusCode = res.statusCode;
 
-    if (statusCode !== 200) {
-      console.error(`Failed to load: ${url}`);
-      res.resume();
-      reject();
-    } else {
-      res.setEncoding('utf8');
-      let responseData = '';
-      res.on('data', chunk => responseData += chunk);
-      res.on('end', () => {
-        resolve(JSON.parse(responseData));
+        if (statusCode !== 200) {
+          console.error(`Failed to load: ${url}`);
+          res.resume();
+          reject();
+        } else {
+          res.setEncoding('utf8');
+          let responseData = '';
+          res.on('data', chunk => (responseData += chunk));
+          res.on('end', () => {
+            try {
+              const parsedData = JSON.parse(responseData);
+              resolve(parsedData);
+            } catch (err) {
+              console.error(`Failed to parse data: ${err.message}`);
+              reject();
+            }
+          });
+        }
+      })
+      .on('error', err => {
+        console.error(`Get request failed: ${err.message}`);
+        reject();
       });
-    }
   });
-});
 
 module.exports = fetch;
